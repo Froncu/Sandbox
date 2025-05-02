e3cea49aa54ab51d9676d59780297a5749b1d501
Jakub Fratczak <jakubfratczak204@gmail.com>
2025-05-02 19:49:30 +0200

    [TEST] check the finished grouping system

--- source/sandbox.cpp
+++ source/sandbox.cpp
@@ -1,12 +1,8 @@
-#include "Sandbox.hpp"
-
-#if defined FRO_DEBUG
-#include <vld.h>
-#endif
+#include "sandbox.hpp"
 
 namespace fro
 {
-   std::unique_ptr<Application> createApplication()
+   std::unique_ptr<Application> create_application()
    {
       return std::make_unique<sbx::Sandbox>();
    }
@@ -16,43 +12,63 @@ namespace sbx
 {
    Sandbox::Sandbox()
    {
-      mSystemEventManager.mInputEvent.addListener(mInputManager.mOnInputEvent);
-      mSystemEventManager.mWindowEvent.addListener(mMainWindow.mOnWindowEvent);
-      fro::Logger::info("created Sandbox!");
+      fro::Locator::set<fro::Logger, fro::ColouredLogger>();
+      fro::Locator::set<fro::SystemEventDispatcher, fro::RegularSystemEventManager>();
+
+      fro::Locator::get<fro::Logger>().info("created Sandbox!");
    }
 
    Sandbox::~Sandbox()
    {
-      fro::Logger::info("destroyed Sandbox!");
+      fro::Locator::reset<fro::SystemEventDispatcher>();
+
+      fro::Locator::get<fro::Logger>().info("destroying Sandbox!");
    }
 
    void Sandbox::run()
    {
-      auto constexpr fixedDeltaSeconds{ 1.0 / 60 };
-      double fixedUpdateAccumulator{};
+      fro::Scene scene{};
+      auto& group{ scene.group<fro::TemplateParameterPack<int, float>, fro::TemplateParameterPack<char>>() };
 
-      auto t1{ std::chrono::steady_clock::now() };
-      while (mIsRunning)
-      {
-         auto const t2{ std::chrono::steady_clock::now() };
-         double const deltaSeconds{ std::chrono::duration<double>(t2 - t1).count() };
-         t1 = t2;
+      fro::Entity& entity_1{ scene.create_entity() };
+      entity_1.add_component<int>(1);
+      entity_1.add_component<float>(1.0f);
+      entity_1.add_component<char>('1');
 
-         mInputManager.processInputContinous();
-         mSystemEventManager.pollEvents();
+      fro::Entity& entity_2{ scene.create_entity() };
+      entity_2.add_component<int>(2);
+      entity_2.add_component<float>(2.0f);
 
-         fixedUpdateAccumulator += deltaSeconds;
-         while (fixedUpdateAccumulator >= fixedDeltaSeconds)
-         {
-            fixedUpdateAccumulator -= fixedDeltaSeconds;
-            // TODO: add physics code
-         }
+      fro::Entity& entity_3{ scene.create_entity() };
+      entity_3.add_component<int>(3);
+      entity_3.add_component<float>(3.0f);
+      entity_3.add_component<char>('3');
 
-         mRenderer.clear();
-         fro::SpriteSystem::onRender(mRenderer);
-         mRenderer.present();
+      fro::Entity& entity_4{ scene.create_entity() };
+      entity_4.add_component<int>(4);
+      entity_4.add_component<float>(4.0f);
+      entity_4.add_component<char>('4');
 
-         fro::SceneManager::doomAndAdd();
-      }
+      scene.execute_queued();
+
+      for (auto&& [entity, integer, character, floating] : group)
+         fro::Locator::get<fro::Logger>().info("integer: {}, character: {}, floating: {}",
+            integer,
+            character,
+            floating);
+
+      scene.destroy_entity(entity_1);
+
+      scene.execute_queued();
+      scene.execute_queued();
+
+      for (auto&& [entity, integer, character, floating] : group)
+         fro::Locator::get<fro::Logger>().info("integer: {}, character: {}, floating: {}",
+            integer,
+            character,
+            floating);
+
+      while (run_)
+         fro::Locator::get<fro::SystemEventDispatcher>().poll_events();
    }
 }
